c900ccc02a25880cb9f3437bf3a29f108a62faa3
zuick <izuick@gmail.com>
2017-03-29 00:58:00 +0500

    load tileset

--- app/js/states/game.js
+++ app/js/states/game.js
@@ -1,10 +1,15 @@
 module.exports = function(game){
   return {
     preload: function() {
-      game.load.image('guy', 'assets/guy.png');
+      game.load.tilemap('map', 'assets/levels/level-1.json', null, Phaser.Tilemap.TILED_JSON);
+      game.load.image('tilemap', 'assets/tilemap.png');
     },
     create: function() {
-      game.add.sprite(0, 0, 'guy');
+      var map = game.add.tilemap('map');
+      map.addTilesetImage('tilemap', 'tilemap');
+      var background = map.createLayer('background');
+      var walls = map.createLayer('walls');
+      background.resizeWorld();
     }
   }
 }
