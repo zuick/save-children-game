4b876c1ef38adbd2a6c30d4ec3bdd8085cb93891
Ruslan Khakimov <izuick@gmail.com>
2017-03-30 18:59:14 +0500

    collision

--- app/js/main.js
+++ app/js/main.js
@@ -2,8 +2,8 @@
   window.PIXI = require('phaser/build/custom/pixi');
   window.p2 = require('phaser/build/custom/p2');
   window.Phaser = require('phaser/build/custom/phaser-split');
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-canvas');
-  var gameState = require('./states/game')(game);
+  var game = new Phaser.Game(800, 600, window.Phaser.AUTO, 'game-canvas');
+  var gameState = require('./states/game')(game, window.Phaser);
 
   game.state.add('game', gameState);
   game.state.start('game');
--- app/js/states/game.js
+++ app/js/states/game.js
@@ -1,15 +1,51 @@
-module.exports = function(game){
+module.exports = function(game, Phaser){
+  var map;
+  var guy;
+  var walls;
+  var background;
   return {
     preload: function() {
       game.load.tilemap('map', 'assets/levels/level-1.json', null, Phaser.Tilemap.TILED_JSON);
       game.load.image('tilemap', 'assets/tilemap.png');
+      game.load.image('guy', 'assets/guy.png');
     },
     create: function() {
-      var map = game.add.tilemap('map');
+      game.physics.startSystem(Phaser.Physics.ARCADE);
+
+      map = game.add.tilemap('map');
+
       map.addTilesetImage('tilemap', 'tilemap');
-      var background = map.createLayer('background');
-      var walls = map.createLayer('walls');
+      map.setCollision(3, true, 'walls');
+
+      background = map.createLayer('background');
+      walls = map.createLayer('walls');
       background.resizeWorld();
+
+      guy = game.add.sprite(16, 16, 'guy');
+      game.physics.enable(guy);
+      guy.body.setCircle(8);
+      guy.body.collideWorldBounds = true;
+
+      cursors = game.input.keyboard.createCursorKeys();
+    },
+    update: function(){
+      var speed = 50;
+      guy.body.velocity.x = 0;
+      guy.body.velocity.y = 0;
+
+      if (cursors.up.isDown){
+        guy.body.velocity.y = -speed;
+      }
+      if (cursors.down.isDown){
+        guy.body.velocity.y = speed;
+      }
+      if (cursors.left.isDown){
+        guy.body.velocity.x = -speed;
+      }
+      if (cursors.right.isDown){
+        guy.body.velocity.x = speed;
+      }
+      game.physics.arcade.collide(guy, walls);
     }
   }
 }
