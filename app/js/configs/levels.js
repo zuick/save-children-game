9e916bb6f4df2bff2011ca7c1d29fae64f01a4b8
a.subbota <a.subbota@corp.nekki.ru>
2017-10-08 14:53:36 +0500

    Список васех уровней, пнг куста 1х1, новый уровень

--- app/js/configs/levels.js
+++ app/js/configs/levels.js
@@ -9,120 +9,240 @@ module.exports = [
       type: 0
     },
     {
-      src: 'assets/levels/test.json',
+      src: 'assets/levels/Level_3.json',
+      type: 0
+    },
+    {
+      src: 'assets/levels/Level_4.json',
+      type: 0
+    },
+    {
+      src: 'assets/levels/Level_5.json',
+      type: 0
+    },
+    {
+      src: 'assets/levels/Level_6.json',
+      type: 0
+    },
+    {
+      src: 'assets/levels/Level_7.json',
+      type: 0
+    },
+    {
+      src: 'assets/levels/Level_8.json',
       type: 1
     },
     {
-      src: 'assets/levels/test.json',
+      src: 'assets/levels/Level_9.json',
       type: 1
     },
     {
-      src: 'assets/levels/test.json',
+      src: 'assets/levels/Level_10.json',
       type: 1
     },
     {
-      src: 'assets/levels/test.json',
+      src: 'assets/levels/Level_11.json',
+      type: 1
+    },
+    {
+      src: 'assets/levels/Level_12.json',
+      type: 1
+    },
+    {
+      src: 'assets/levels/Level_13.json',
+      type: 1
+    },
+    {
+      src: 'assets/levels/Level_14.json',
+      type: 1
+    },
+    {
+      src: 'assets/levels/Level_15.json',
       type: 2
     },
     {
-      src: 'assets/levels/test.json',
+      src: 'assets/levels/Level_16.json',
       type: 2
     },
     {
-      src: 'assets/levels/test.json',
+      src: 'assets/levels/Level_17.json',
       type: 2
     },
     {
-      src: 'assets/levels/test.json',
+      src: 'assets/levels/Level_18.json',
       type: 2
     },
     {
-      src: 'assets/levels/test.json',
+      src: 'assets/levels/Level_19.json',
+      type: 2
+    },
+    {
+      src: 'assets/levels/Level_20.json',
       type: 2
     }
   ],
   [
     {
-      src: 'assets/levels/Level_1.json',
-      type: 2
-    },
-    {
-      src: 'assets/levels/Level_2.json',
+      src: 'assets/levels/Level_21.json',
       type: 0
     },
     {
-      src: 'assets/levels/test.json',
-      type: 2
-    },
-    {
-      src: 'assets/levels/test.json',
+      src: 'assets/levels/Level_22.json',
       type: 0
     },
     {
-      src: 'assets/levels/test.json',
-      type: 2
-    },
-    {
-      src: 'assets/levels/test.json',
+      src: 'assets/levels/Level_23.json',
       type: 0
     },
     {
-      src: 'assets/levels/test.json',
-      type: 2
-    },
-    {
-      src: 'assets/levels/test.json',
+      src: 'assets/levels/Level_24.json',
       type: 0
     },
     {
-      src: 'assets/levels/test.json',
+      src: 'assets/levels/Level_25.json',
+      type: 0
+    },
+    {
+      src: 'assets/levels/Level_26.json',
+      type: 0
+    },
+    {
+      src: 'assets/levels/Level_27.json',
+      type: 0
+    },
+    {
+      src: 'assets/levels/Level_28.json',
+      type: 1
+    },
+    {
+      src: 'assets/levels/Level_29.json',
+      type: 1
+    },
+    {
+      src: 'assets/levels/Level_30.json',
+      type: 1
+    },
+    {
+      src: 'assets/levels/Level_31.json',
+      type: 1
+    },
+    {
+      src: 'assets/levels/Level_32.json',
+      type: 1
+    },
+    {
+      src: 'assets/levels/Level_33.json',
+      type: 1
+    },
+    {
+      src: 'assets/levels/Level_34.json',
+      type: 1
+    },
+    {
+      src: 'assets/levels/Level_35.json',
       type: 2
     },
     {
-      src: 'assets/levels/test.json',
-      type: 0
+      src: 'assets/levels/Level_36.json',
+      type: 2
+    },
+    {
+      src: 'assets/levels/Level_37.json',
+      type: 2
+    },
+    {
+      src: 'assets/levels/Level_38.json',
+      type: 2
+    },
+    {
+      src: 'assets/levels/Level_39.json',
+      type: 2
+    },
+    {
+      src: 'assets/levels/Level_40.json',
+      type: 2
     }
   ],
   [
     {
-      src: 'assets/levels/Level_1.json',
-      type: 1
-    },
-    {
-      src: 'assets/levels/Level_2.json',
+      src: 'assets/levels/Level_41.json',
       type: 0
     },
     {
-      src: 'assets/levels/test.json',
-      type: 1
-    },
-    {
-      src: 'assets/levels/test.json',
+      src: 'assets/levels/Level_42.json',
       type: 0
     },
     {
-      src: 'assets/levels/test.json',
-      type: 1
-    },
-    {
-      src: 'assets/levels/test.json',
+      src: 'assets/levels/Level_43.json',
       type: 0
     },
     {
-      src: 'assets/levels/test.json',
-      type: 1
-    },
-    {
-      src: 'assets/levels/test.json',
+      src: 'assets/levels/Level_44.json',
       type: 0
     },
     {
-      src: 'assets/levels/test.json',
+      src: 'assets/levels/Level_45.json',
+      type: 0
+    },
+    {
+      src: 'assets/levels/Level_46.json',
+      type: 0
+    },
+    {
+      src: 'assets/levels/Level_47.json',
+      type: 0
+    },
+    {
+      src: 'assets/levels/Level_48.json',
       type: 1
     },
     {
-      src: 'assets/levels/test.json',
-      type: 0
+      src: 'assets/levels/Level_49.json',
+      type: 1
+    },
+    {
+      src: 'assets/levels/Level_50.json',
+      type: 1
+    },
+    {
+      src: 'assets/levels/Level_51.json',
+      type: 1
+    },
+    {
+      src: 'assets/levels/Level_52.json',
+      type: 1
+    },
+    {
+      src: 'assets/levels/Level_53.json',
+      type: 1
+    },
+    {
+      src: 'assets/levels/Level_54.json',
+      type: 1
+    },
+    {
+      src: 'assets/levels/Level_55.json',
+      type: 2
+    },
+    {
+      src: 'assets/levels/Level_56.json',
+      type: 2
+    },
+    {
+      src: 'assets/levels/Level_57.json',
+      type: 2
+    },
+    {
+      src: 'assets/levels/Level_58.json',
+      type: 2
+    },
+    {
+      src: 'assets/levels/Level_59.json',
+      type: 2
+    },
+    {
+      src: 'assets/levels/Level_60.json',
+      type: 2
     }
   ]
 ]
