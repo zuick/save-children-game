1b8924637fa07ce1384e820f6a0542a51cb76454
zuick <izuick@gmail.com>
2017-10-12 20:21:56 +0500

    fix child in "no way" case

--- app/js/modules/stray.js
+++ app/js/modules/stray.js
@@ -84,6 +84,8 @@ module.exports = function(game, Phaser){
               }else if(!canMoveForward && canMoveBackward){
                 currentDir = backwardDir;
                 delta = this.getDeltaTo(nextTile, true);
+              }else{
+                currentDir = void 0;
               }
             }
           }
