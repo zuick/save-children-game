d2c33758de480d84161aa7d2c18401c013b8901a
a.subbota <a.subbota@corp.nekki.ru>
2017-10-16 21:07:51 +0500

    Локали баш и рус

--- app/js/configs/languages/ba.js
+++ app/js/configs/languages/ba.js
@@ -1,5 +1,13 @@
 module.exports = {
-  "DIFFICULTY_LEVEL": "ҠАТМАРЛЫЛЫҠ КИМӘЛЕ: {0}",
-  "LEVEL_NUMBER": "КИМӘЛ {0}-{1}",
-  "PAUSE": "Тынлыҡ"
+  "DIFFICULTY_LEVEL": "Ҡатмарлылыҡ кимәле: {0}",
+  "LEVEL_NUMBER": "Кимәл {0}-{1}",
+  "PAUSE": "Тынлыҡ",
+  "CONGRATULATIONS": "Еңеү!",
+  "FAIL": "Еңелеү",
+  "TIME": "Ваҡыт: {0}",
+  "STAT": "Ҡотҡарылды: {0}/{1}",
+  "MENU": "МЕНЮ",
+  "REPLAY": "Ҡабат уйнау",
+  "LEVELS": "Кимәлдәр",
+  "NEXT": "Артабан"
 }
--- app/js/configs/languages/ru.js
+++ app/js/configs/languages/ru.js
@@ -2,8 +2,12 @@ module.exports = {
   "DIFFICULTY_LEVEL": "Уровень сложности: {0}",
   "LEVEL_NUMBER": "Уровень {0}-{1}",
   "PAUSE": "Пауза",
-  "CONGRATULATIONS": "Поздравляем!",
-  "FAIL": "Поражение :(",
+  "CONGRATULATIONS": "Победа!",
+  "FAIL": "Поражение",
   "TIME": "Время: {0}",
-  "STAT": "Спасено детей: {0}/{1}"
+  "STAT": "Спасено детей: {0}/{1}",
+  "MENU": "МЕНЮ",
+  "REPLAY": "Переиграть",
+  "LEVELS": "Уровни",
+  "NEXT": "Далее"
 }
