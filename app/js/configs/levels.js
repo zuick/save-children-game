ac024b18ab77267671d93f228c08271b84ca72e3
a.subbota <a.subbota@corp.nekki.ru>
2017-10-25 15:45:26 +0500

    титулы и их время

--- app/js/configs/levels.js
+++ app/js/configs/levels.js
@@ -4,306 +4,365 @@ module.exports = [
       src: 'assets/levels/Level_1.json',
       type: 1,
       bonusDelay: 10,
-      titlesTime: [5, 8, 11]
+      titlesTime: [1, 5, 8]
     },
     {
       src: 'assets/levels/Level_2.json',
       type: 1,
-      bonusDelay: 10
+      bonusDelay: 10,
+      titlesTime: [1, 7, 10]
     },
     {
       src: 'assets/levels/Level_3.json',
       type: 1,
-      bonusDelay: 10
+      bonusDelay: 10,
+      titlesTime: [6, 8, 12]
     },
     {
       src: 'assets/levels/Level_4.json',
       type: 1,
-      bonusDelay: 10
+      bonusDelay: 10,
+      titlesTime: [6, 8, 11]
     },
     {
       src: 'assets/levels/Level_5.json',
       type: 1,
-      bonusDelay: 13
+      bonusDelay: 13,
+      titlesTime: [8, 11, 14]
     },
     {
       src: 'assets/levels/Level_6.json',
       type: 1,
-      bonusDelay: 10
+      bonusDelay: 10,
+      titlesTime: [6, 9, 13]
     },
     {
       src: 'assets/levels/Level_7.json',
       type: 1,
-      bonusDelay: 14
+      bonusDelay: 14,
+      titlesTime: [8, 11, 14]
     },
     {
       src: 'assets/levels/Level_8.json',
       type: 0,
-      bonusDelay: 17
+      bonusDelay: 17,
+      titlesTime: [14, 17, 22]
     },
     {
       src: 'assets/levels/Level_9.json',
       type: 0,
-      bonusDelay: 13
+      bonusDelay: 13,
+      titlesTime: [10, 13, 17]
     },
     {
       src: 'assets/levels/Level_10.json',
       type: 0,
-      bonusDelay: 14
+      bonusDelay: 14,
+      titlesTime: [11, 14, 20]
     },
     {
       src: 'assets/levels/Level_11.json',
       type: 0,
-      bonusDelay: 18
+      bonusDelay: 18,
+      titlesTime: [10, 18, 25]
     },
     {
       src: 'assets/levels/Level_12.json',
       type: 0,
-      bonusDelay: 13
+      bonusDelay: 13,
+      titlesTime: [8, 13, 17]
     },
     {
       src: 'assets/levels/Level_13.json',
       type: 0,
-      bonusDelay: 14
+      bonusDelay: 14,
+      titlesTime: [11, 14, 19]
     },
     {
       src: 'assets/levels/Level_14.json',
       type: 0,
-      bonusDelay: 20
+      bonusDelay: 20,
+      titlesTime: [12, 20, 25]
     },
     {
       src: 'assets/levels/Level_15.json',
       type: 2,
-      bonusDelay: 15
+      bonusDelay: 15,
+      titlesTime: [12, 15, 19]
     },
     {
       src: 'assets/levels/Level_16.json',
       type: 2,
-      bonusDelay: 18
+      bonusDelay: 18,
+      titlesTime: [14, 18, 23]
     },
     {
       src: 'assets/levels/Level_17.json',
       type: 2,
-      bonusDelay: 22
+      bonusDelay: 22,
+      titlesTime: [18, 22, 26]
     },
     {
       src: 'assets/levels/Level_18.json',
       type: 2,
-      bonusDelay: 11
+      bonusDelay: 11,
+      titlesTime: [7, 11, 16]
     },
     {
       src: 'assets/levels/Level_19.json',
       type: 2,
-      bonusDelay: 12
+      bonusDelay: 12,
+      titlesTime: [11, 15, 21]
     },
     {
       src: 'assets/levels/Level_20.json',
       type: 2,
-      bonusDelay: 18
+      bonusDelay: 18,
+      titlesTime: [13, 19, 26]
     }
   ],
   [
     {
       src: 'assets/levels/Level_21.json',
       type: 1,
-      bonusDelay: 23
+      bonusDelay: 23,
+      titlesTime: [19, 23, 28]
     },
     {
       src: 'assets/levels/Level_22.json',
       type: 1,
-      bonusDelay: 21
+      bonusDelay: 21,
+      titlesTime: [18, 22, 28]
     },
     {
       src: 'assets/levels/Level_23.json',
       type: 1,
-      bonusDelay: 41
+      bonusDelay: 41,
+      titlesTime: [35, 41, 48]
     },
     {
       src: 'assets/levels/Level_24.json',
       type: 1,
-      bonusDelay: 35
+      bonusDelay: 35,
+      titlesTime: [30, 35, 41]
     },
     {
       src: 'assets/levels/Level_25.json',
       type: 1,
-      bonusDelay: 28
+      bonusDelay: 28,
+      titlesTime: [22, 28, 34]
     },
     {
       src: 'assets/levels/Level_26.json',
       type: 1,
-      bonusDelay: 25
+      bonusDelay: 25,
+      titlesTime: [20, 25, 31]
     },
     {
       src: 'assets/levels/Level_27.json',
       type: 1,
-      bonusDelay: 34
+      bonusDelay: 34,
+      titlesTime: [30, 34, 39]
     },
     {
       src: 'assets/levels/Level_28.json',
       type: 0,
-      bonusDelay: 17
+      bonusDelay: 17,
+      titlesTime: [13, 17, 21]
     },
     {
       src: 'assets/levels/Level_29.json',
       type: 0,
-      bonusDelay: 20
+      bonusDelay: 20,
+      titlesTime: [16, 21, 27]
     },
     {
       src: 'assets/levels/Level_30.json',
       type: 0,
-      bonusDelay: 22
+      bonusDelay: 22,
+      titlesTime: [18, 22, 27]
     },
     {
       src: 'assets/levels/Level_31.json',
       type: 0,
-      bonusDelay: 28
+      bonusDelay: 28,
+      titlesTime: [23, 28, 36]
     },
     {
       src: 'assets/levels/Level_32.json',
       type: 0,
-      bonusDelay: 26
+      bonusDelay: 26,
+      titlesTime: [18, 26, 38]
     },
     {
       src: 'assets/levels/Level_33.json',
       type: 0,
-      bonusDelay: 30
+      bonusDelay: 30,
+      titlesTime: [21, 30, 39]
     },
     {
       src: 'assets/levels/Level_34.json',
       type: 0,
-      bonusDelay: 32
+      bonusDelay: 32,
+      titlesTime: [18, 32, 42]
     },
     {
       src: 'assets/levels/Level_35.json',
       type: 2,
-      bonusDelay: 50
+      bonusDelay: 50,
+      titlesTime: [40, 50, 60]
     },
     {
       src: 'assets/levels/Level_36.json',
       type: 2,
-      bonusDelay: 28
+      bonusDelay: 28,
+      titlesTime: [23, 28, 33]
     },
     {
       src: 'assets/levels/Level_37.json',
       type: 2,
-      bonusDelay: 42
+      bonusDelay: 42,
+      titlesTime: [32, 42, 50]
     },
     {
       src: 'assets/levels/Level_38.json',
       type: 2,
-      bonusDelay: 20
+      bonusDelay: 20,
+      titlesTime: [14, 20, 25]
     },
     {
       src: 'assets/levels/Level_39.json',
       type: 2,
-      bonusDelay: 33
+      bonusDelay: 33,
+      titlesTime: [25, 33, 40]
     },
     {
       src: 'assets/levels/Level_40.json',
       type: 2,
-      bonusDelay: 23
+      bonusDelay: 23,
+      titlesTime: [18, 23, 28]
     }
   ],
   [
     {
       src: 'assets/levels/Level_41.json',
       type: 1,
-      bonusDelay: 50
+      bonusDelay: 50,
+      titlesTime: [43, 50, 56]
     },
     {
       src: 'assets/levels/Level_42.json',
       type: 1,
-      bonusDelay: 30
+      bonusDelay: 30,
+      titlesTime: [25, 30, 35]
     },
     {
       src: 'assets/levels/Level_43.json',
       type: 1,
-      bonusDelay: 42
+      bonusDelay: 42,
+      titlesTime: [30, 42, 50]
     },
     {
       src: 'assets/levels/Level_44.json',
       type: 1,
-      bonusDelay: 35
+      bonusDelay: 35,
+      titlesTime: [29, 35, 41]
     },
     {
       src: 'assets/levels/Level_45.json',
       type: 1,
-      bonusDelay: 40
+      bonusDelay: 40,
+      titlesTime: [33, 40, 46]
     },
     {
       src: 'assets/levels/Level_46.json',
       type: 1,
-      bonusDelay: 55
+      bonusDelay: 55,
+      titlesTime: [45, 55, 62]
     },
     {
       src: 'assets/levels/Level_47.json',
       type: 1,
-      bonusDelay: 68
+      bonusDelay: 68,
+      titlesTime: [58, 68, 75]
     },
     {
       src: 'assets/levels/Level_48.json',
       type: 0,
-      bonusDelay: 47
+      bonusDelay: 47,
+      titlesTime: [37, 47, 56]
     },
     {
       src: 'assets/levels/Level_49.json',
       type: 0,
-      bonusDelay: 46
+      bonusDelay: 46,
+      titlesTime: [34, 46, 54]
     },
     {
       src: 'assets/levels/Level_50.json',
       type: 0,
-      bonusDelay: 57
+      bonusDelay: 57,
+      titlesTime: [47, 57, 67]
     },
     {
       src: 'assets/levels/Level_51.json',
       type: 0,
-      bonusDelay: 45
+      bonusDelay: 45,
+      titlesTime: [35, 45, 54]
     },
     {
       src: 'assets/levels/Level_52.json',
       type: 0,
-      bonusDelay: 80
+      bonusDelay: 80,
+      titlesTime: [70, 80, 90]
     },
     {
       src: 'assets/levels/Level_53.json',
       type: 0,
-      bonusDelay: 41
+      bonusDelay: 41,
+      titlesTime: [29, 41, 50]
     },
     {
       src: 'assets/levels/Level_54.json',
       type: 0,
-      bonusDelay: 52
+      bonusDelay: 52,
+      titlesTime: [45, 52, 60]
     },
     {
       src: 'assets/levels/Level_55.json',
       type: 2,
-      bonusDelay: 42
+      bonusDelay: 42,
+      titlesTime: [31, 42, 50]
     },
     {
       src: 'assets/levels/Level_56.json',
       type: 2,
-      bonusDelay: 75
+      bonusDelay: 75,
+      titlesTime: [65, 75, 84]
     },
     {
       src: 'assets/levels/Level_57.json',
       type: 2,
-      bonusDelay: 80
+      bonusDelay: 80,
+      titlesTime: [70, 80, 89]
     },
     {
       src: 'assets/levels/Level_58.json',
       type: 2,
-      bonusDelay: 45
+      bonusDelay: 45,
+      titlesTime: [34, 45, 52]
     },
     {
       src: 'assets/levels/Level_59.json',
       type: 2,
-      bonusDelay: 45
+      bonusDelay: 45,
+      titlesTime: [34, 45, 52]
     },
     {
       src: 'assets/levels/Level_60.json',
       type: 2,
-      bonusDelay: 85
+      bonusDelay: 85,
+      titlesTime: [75, 85, 95]
     }
   ]
 ]
